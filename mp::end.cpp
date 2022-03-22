40c6556fe7754a1e25a6b3b882e9a807101f9dc7
Manu Pratap <70874605+martial-manu@users.noreply.github.com>
2022-03-23 00:05:09 +0530

    Create mp::end.cpp

---
+++ mp::end.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    map<int,int>mp = {
+        {1,2} , {2,3} , {5 , 6}
+    };
+
+    if(mp.find(3)==mp.end())cout << "lol";
+    if(mp.find(3)==mp.end())cout << "lol";
+
+}
