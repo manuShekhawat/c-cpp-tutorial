12340c023f797d8aa5be474f9e06a82d2f530503
Manu Pratap <70874605+martial-manu@users.noreply.github.com>
2022-12-10 18:53:30 +0530

    Update manu.cpp

--- manu.cpp
+++ manu.cpp
@@ -523,3 +523,20 @@ vector<int>fact(N+1);
     return (((fact[a]*inv[b])%p)*inv[a-b])%p; 
   };
 
+
+///////////////////////////////////  Fenwick tree implementation /////////////////////////////////////////////////////////////////////////////////////
+
+
+struct FT {
+	vector<ll> s;
+	FT(int n) : s(n) {}
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
+	}
+	ll query(int pos) { // sum of values in [0, pos)
+		ll res = 0;
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+		return res;
+	}
+};
+
